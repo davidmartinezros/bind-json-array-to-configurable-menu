f5cde2b05d5d069ef36ea1d32f21f85e28c85a41
David Martinez Ros <davidnezan@gmail.com>
2019-03-01 13:18:27 +0100

    first version of horizontal menu bar with json and style files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ReturnJsonArrayService } from './return-json-array.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'bind-json-array-to-configurable-menu';
+
+  data: Observable<Array<any>>;
+
+  constructor(private service: ReturnJsonArrayService) {
+    this.data = service.getMenu();
+    console.log("AppComponent.data:" + this.data);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ReturnJsonArrayService } from './return-json-array.service';
+import { HttpClientModule }    from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule
   ],
-  providers: [],
+  providers: [ReturnJsonArrayService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
